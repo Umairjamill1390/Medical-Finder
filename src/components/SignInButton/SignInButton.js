5e2af7642193b9d26c495ccff9431319a9cb113a
Umair Jamil <umairjamill1390@gmail.com>
2023-11-07 23:36:01 -0500

    - Created a SignInButton component - Added CSS styles to SignInButton

---
+++ src/components/SignInButton/SignInButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SignInButton.css'; 
+
+function SignInButton() {
+    return (
+        <button className="btn btn-primary sign-in-btn mx-3">Sign In</button>
+    );
+}
+
+export default SignInButton;
