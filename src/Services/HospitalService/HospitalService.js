132d678f308f817cde7f05d3fc0c0b8a5b320f1f
Umair Jamil <umairjamill1390@gmail.com>
2023-11-14 19:41:30 -0500

    - Updating Hospital Cards wit Google maps link

--- src/Services/HospitalService/HospitalService.js
+++ src/Services/HospitalService/HospitalService.js
@@ -27,7 +27,8 @@ export const getCoordinatesFromZip = async (zipCode) => {
 // Function to get hospitals from the Overpass API based on coordinates
 export const getHospitalsNearby = async ({ latitude, longitude }, distance) => {
     const overpassQuery = `
-    [out:json];
+    [out:json]; 
+    // add more data from overpass 
     (
         node["amenity"="hospital"](around:${distance * 1609.34},${latitude},${longitude}); // distance * 1609.34 to convert from miles to meters
     );
@@ -47,10 +48,36 @@ export const getHospitalsNearby = async ({ latitude, longitude }, distance) => {
     }));
 };
 
+export const getHospitalDetails = async (hospitalName) => {
+    // Construct the URL for the Hospital Compare API with the hospital name
+    const url = `https://hospital-compare-api.com/details?name=${encodeURIComponent(hospitalName)}`;
+    // Replace with the actual endpoint and parameters
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data; // Modify based on the actual response structure
+};
 
 // The onSearch function that uses the above functions
 export const onSearch = async (zipCode, distance) => {
     const coordinates = await getCoordinatesFromZip(zipCode);
     const hospitals = await getHospitalsNearby(coordinates, distance);
-    return hospitals;
+
+    const detailedHospitals = await Promise.all(
+        hospitals.map(async (hospital) => {
+            try {
+                const details = await getHospitalDetails(hospital.name);
+                return { ...hospital, ...details }; // Combine basic and detailed data
+            } catch (error) {
+                console.error('Error fetching details for hospital:', hospital.name);
+                return hospital; // Return basic data if detailed data can't be fetched
+            }
+        })
+    );
+
+    return detailedHospitals;
 };
