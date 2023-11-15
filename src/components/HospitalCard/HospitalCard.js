128b7ddc83ac07bdf0b27d38f582465458565c3d
Umair Jamil <umairjamill1390@gmail.com>
2023-11-14 19:41:30 -0500

    - Updating Hospital Cards wit Google maps link

--- src/components/HospitalCard/HospitalCard.js
+++ src/components/HospitalCard/HospitalCard.js
@@ -5,9 +5,15 @@ import './HospitalCard.css';
 import defaultImage from '../../Assets/hospital-default.png'; // Adjust the path as necessary
 
 function HospitalCard({ hospital }) {
+  const openInGoogleMaps = () => {
+    const query = hospital.name; // Replace with coordinates if available
+    const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}`;
+    window.open(url, '_blank');
+  };
+
   return (
-    <div className="hospital-card">
-      <div className="background-image" style={{ backgroundImage: `url(${hospital.imageUrl || defaultImage})` }}></div>
+    <div className="hospital-card" onClick={openInGoogleMaps} style={{ backgroundImage: `url(${hospital.imageUrl || defaultImage})` }}>
+      <div className="background-image"></div>
       <div className="card-content">
         <h5 className="hospital-name">{hospital.name}</h5>
         <p className="hospital-distance">{hospital.distance} miles away</p>
